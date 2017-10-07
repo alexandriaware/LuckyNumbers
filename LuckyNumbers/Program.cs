e6f66549e82b9cc5e63b62746903e03cde0992e1
alexandriaware <ali.mercer92@gmail.com>
2017-10-07 18:18:58 -0400

    finish

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -11,96 +11,118 @@ namespace LuckyNumbers
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Welcome to Lucky Numbers! Here's how the game is played: \nYou pick a range between two numbers, like 1 and 13. Then, you\n pick six numbers within that range.");
-            Console.WriteLine("Your opponent, the computer, will also pick six numbers within your range. If \nyou guess all six of the computer's numbers correctly, you will win $60,000!");
-            int firstNum;
-            int secondNum;
+            string playAgain = "";
 
-            //continue asking for numbers until they are not equal to each other
             do
             {
-                Console.WriteLine("Please enter the first number in your range:");
-                firstNum = int.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine("Welcome to Lucky Numbers! Here's how the game is played: \nYou pick a range between two numbers, like 1 and 13. Then, you \npick six numbers within that range.");
+                Console.WriteLine("Your opponent, the computer, will also pick six numbers within your range. If \nyou guess all six of the computer's numbers correctly, you will win $60,000!");
+                Console.WriteLine("Are you ready to play? \n");
 
-                Console.WriteLine("Please enter the second number in your range:");
-                secondNum = int.Parse(Console.ReadLine());
-                if (secondNum <= firstNum)
+                int firstNum;
+                int secondNum;
+
+                //continue asking for numbers until they are not equal to each other
+                do
                 {
-                    Console.WriteLine("Invalid. Please enter two different numbers.\n");
+                    Console.WriteLine("Please enter the first number in your range:");
+                    firstNum = int.Parse(Console.ReadLine());
 
+                    Console.WriteLine("Please enter the second number in your range:");
+                    secondNum = int.Parse(Console.ReadLine());
+                    if (secondNum <= firstNum)
+                    {
+                        Console.WriteLine("Invalid. Please enter two different numbers.\n");
+
+                    }
                 }
-            }
-            while (secondNum <= firstNum);
+                while (secondNum <= firstNum);
 
-            Console.WriteLine("Thank you.");
+                Console.WriteLine("Thank you.");
 
 
-            //user array
-            int[] luckyGuess = new int[6];
-            
-            Console.WriteLine("Please guess a number from within your range.\n");
-           
-            for (int i = 0; i < luckyGuess.Length; i++)
-            {
-                luckyGuess[i] = int.Parse(Console.ReadLine());
-                if (luckyGuess[i] < firstNum || luckyGuess[i] > secondNum)
+                //user array
+                int[] luckyGuess = new int[6];
+
+                Console.WriteLine("Please guess a number from within your range.");
+
+                for (int i = 0; i < luckyGuess.Length; i++)
                 {
-                    Console.WriteLine("Please enter a valid number. Pick a number between " + firstNum + " and " + secondNum + ".");
+                    luckyGuess[i] = int.Parse(Console.ReadLine());
+                    if (luckyGuess[i] < firstNum || luckyGuess[i] > secondNum)
+                    {
+                        Console.WriteLine("Please enter a valid number. Pick a number between " + firstNum + " and " + secondNum + ".");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter another number!");
+                    }
+                }
+
+                Console.WriteLine("Thank you!\n");
+
+
+                //winning numbers array
+                int correctGuesses = 0;
+
+                Random rando = new Random();
+                int[] luckyNum = new int[6];
+
+                for (int i = 0; i < luckyNum.Length; i++)
+                {
+                    int winningNumber = rando.Next(secondNum) + 1;
+                    luckyNum[i] = winningNumber;
+                }
+
+                foreach (int winningNumber in luckyNum)
+                {
+                    Console.WriteLine("Lucky Number: " + winningNumber);
+                }
+
+
+                //compare the user and computer's arrays
+                foreach (int number in luckyGuess)
+                {
+                    if (luckyNum.Contains(number))
+                    {
+                        correctGuesses++;
+                    }
+                }
+
+                Console.WriteLine("You guessed " + correctGuesses + " correctly!");
+
+
+
+                //jackpot is equal to $60,000
+                int jackpot = 60000;
+                int prizeMoney = correctGuesses * 10000;
+
+                if (correctGuesses == 6)
+                {
+                    Console.WriteLine("CONGRATULATIONS! You guessed them all right! $60,000 to you!");
+                }
+                else if (correctGuesses == 0)
+                {
+                    Console.WriteLine("You lose. No correct guesses.");
                 }
                 else
                 {
-                    Console.WriteLine("Please enter another number!\n");
+                    Console.WriteLine("You have won $" + prizeMoney + "!");
                 }
+
+
+                Console.WriteLine("Would you like to play again?");
+                playAgain = Console.ReadLine().ToLower();
             }
 
-            Console.WriteLine("Thank you!\n");
-
-            //winning numbers array
-            int correctGuesses = 0;
-
-            Random rando = new Random();
-            int[] luckyNum = new int[6];
-
-            for (int i = 0; i < luckyNum.Length; i++)
-            {
-                int randomNumber = rando.Next(secondNum) + 1;
-                luckyNum[i] = randomNumber;
-            }
+            while (playAgain == "yes");
             
-            foreach (int randomNumber in luckyNum)
-            {
-                Console.WriteLine("Lucky Number: " + randomNumber);
-            }
-
-            foreach (int number in luckyGuess)
-            {
-               if (luckyNum.Contains(number))
-                {
-                    correctGuesses++;
-                }
-            }
-
-            Console.WriteLine("You guessed " + correctGuesses + " correctly!");
-
-
-
-            //jackpot is equal to $60,000
-            int jackpot = 60000;
-            int prizeMoney = jackpot / correctGuesses;
-
-            if (correctGuesses == 6)
-            {
-                Console.WriteLine("CONGRATULATIONS! You guessed them all right! $60,000 to you!");
-            }
-            else
-            {
-                Console.WriteLine("You have won $" + prizeMoney + "!");
-            }
 
 
             
-            
-             
+
+
 
 
 
